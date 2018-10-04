0a38d580579be7a4f477bc7b58490864ebb77d04
Arthur Hsieh <31050112+arthur960304@users.noreply.github.com>
2018-10-04 21:44:45 +0800

    Update findPath.h

--- HW2/FindPath/FindPath/findPath.h
+++ HW2/FindPath/FindPath/findPath.h
@@ -17,15 +17,21 @@ struct offsets
     int a, b;
 };
 
+//four directions are available
 enum direct
 {
     E, S, W, N
 };
 
+
 class FindPath
 {
 private:
     int input, width, height;
+    int start_g = 0, start_h = 0;   //starting col and row
+    int exit_g = 0, exit_h = 0;     //ending col and row
+    int i, j, g, h, m, n;           //unimportant factors
+    
     string s_input, s_width, s_height;
     
     ifstream inFile1, inFile2;
@@ -37,5 +43,7 @@ public:
     int firstMethod(string, string);
     int BFS(string, string);
 };
+
+
 
 #endif /* Header_h */
